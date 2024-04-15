4e6af8cb14ee95788b4c326b695f16b21af8e91a
Martin Bradbury <martinbradbury@btinternet.com>
2024-04-15 11:09:15 +0000

    bu1 displays in django admin and superuser can create data.

--- biology_unit1/admin.py
+++ biology_unit1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Bu1
 
 # Register your models here.
+
+admin.site.register(Bu1)
--- biology_unit1/models.py
+++ biology_unit1/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 
 
 class Bu1(models.Model):
-    q1 = models.IntegerField()
+    title = models.CharField(max_length=100)
+    q1 = models.IntegerField(validators=[MaxValueValidator(5)])
     q2 = models.IntegerField()
     q3 = models.IntegerField()
     q4 = models.IntegerField()
     q5 = models.IntegerField()
+
+    def __str__(self):
+        return self.title
